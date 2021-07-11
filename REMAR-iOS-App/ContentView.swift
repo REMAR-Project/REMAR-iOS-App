4f0cf884b6619af6b6155097e616bc37a7030a29
watson <lewiswatson55@hotmail.co.uk>
2021-07-11 18:32:28 +0100

    Initial Commit

---
+++ REMAR-iOS-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  REMAR-iOS-App
+//
+//  Created by Lewis Watson on 11/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ REMAR-iOS-App/REMAR_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  REMAR_iOS_AppApp.swift
+//  REMAR-iOS-App
+//
+//  Created by Lewis Watson on 11/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct REMAR_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
