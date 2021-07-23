4dd1b9915075d310379d48489b0779df3b3d52ef
watson <lewiswatson55@hotmail.co.uk>
2021-07-23 19:02:33 +0100

    Changed from REMAR_iOS_AppApp to REMAR_iOS_App

---
+++ REMAR-iOS-App/REMAR_iOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  REMAR_iOS_App.swift
+//  REMAR-iOS-App
+//
+//  Created by Lewis Watson on 11/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct REMAR_iOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
