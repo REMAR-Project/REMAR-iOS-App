fb000151f0e89a1cd3ab8af16397dc80de174125
watson <lewiswatson55@hotmail.co.uk>
2021-07-23 18:59:07 +0100

    Started Recreating REMAR App UI.
    Set up project, added assets, recreated splash screen

--- REMAR-iOS-App/ContentView.swift
+++ REMAR-iOS-App/ContentView.swift
@@ -2,15 +2,54 @@
 //  ContentView.swift
 //  REMAR-iOS-App
 //
-//  Created by Lewis Watson on 11/07/2021.
+//  Created by Lewis Watson on 23/07/2021.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Rectangle()
+                .edgesIgnoringSafeArea(.top)
+                .foregroundColor(Color("REMAR_GREEN"))
+            VStack{
+                HStack(spacing: 0.0){
+                    Text("REMAR_")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                    Text("CITIZEN")
+                }
+                .foregroundColor(.black)
+                .padding(.top, 20.0)
+                .font(.title)
+                
+                Spacer()
+                Text("Monitoring Mass Mating Events of Mangrove Crabs")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                Image("splashscreen_crabs")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(10.0)
+                Spacer()
+                ZStack{
+                    VStack{
+                        Image("splashscreen_logos1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        Image("splashscreen_logos2")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    .padding(.top, 15.0)
+                    .background(Color(.white))
+                    .preferredColorScheme(.light)
+                }
+            }
+        }
     }
 }
 
--- REMAR-iOS-App/REMAR_iOS_AppApp.swift
+++ REMAR-iOS-App/REMAR_iOS_AppApp.swift
@@ -1,17 +1,1 @@
-//
-//  REMAR_iOS_AppApp.swift
-//  REMAR-iOS-App
-//
-//  Created by Lewis Watson on 11/07/2021.
-//
 
-import SwiftUI
-
-@main
-struct REMAR_iOS_AppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
