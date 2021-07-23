0a8e06e6a2732edcf7562bbe00705b045fc2e2b7
lewiswatson55 <lewiswatson55@protonmail.com>
2021-07-23 19:19:50 +0100

    Refactored splash screen into its own view, fixed GitHub account showing on commits.

---
+++ REMAR-iOS-App/splashscreenView.swift
@@ -0,0 +1,61 @@
+//
+//  splashscreenView.swift
+//  REMAR-iOS-App
+//
+//  Created by Lewis Watson on 23/07/2021.
+//
+
+import SwiftUI
+
+//  This view will be temporarily shown as the app loads.
+struct splashscreenView: View {
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .edgesIgnoringSafeArea(.top)
+                .foregroundColor(Color("REMAR_GREEN"))
+            VStack{
+                HStack(spacing: 0.0){
+                    Text("REMAR_")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                    Text("CITIZEN")
+                }
+                .foregroundColor(.black)
+                .padding(.top, 20.0)
+                .font(.title)
+                
+                Spacer()
+                Text("Monitoring Mass Mating Events of Mangrove Crabs")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                Image("splashscreen_crabs")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(10.0)
+                Spacer()
+                ZStack{
+                    VStack{
+                        Image("splashscreen_logos1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        Image("splashscreen_logos2")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    .padding(.top, 15.0)
+                    .background(Color(.white))
+                    .preferredColorScheme(.light)
+                }
+            }
+        }
+    }
+}
+
+struct splashscreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        splashscreenView()
+    }
+}
