1e030ef280c7fd4474cea5b7bb6b1b53942d5227
lewiswatson55 <lewiswatson55@protonmail.com>
2021-08-22 01:29:42 +0100

    Final submission page view done.

--- REMAR-iOS-App/questionViews/finalSubmissionView.swift
+++ REMAR-iOS-App/questionViews/finalSubmissionView.swift
@@ -14,13 +14,26 @@ struct finalSubmissionView: View {
         GeometryReader { geom in
             ZStack {
                 uiBackgroundView()
-                
                 VStack{
-                    Button(action: {QuestionManager.currentQuestion = 0}, label: {Text("BONK")})
-                }
-                .padding(.bottom)
-                .padding(.top, 50)
-                .ignoresSafeArea()
+                    
+                    
+                    
+                    Text("To view analyses of the information we receive from you and other citizen scientists, please see the REMAR website at\n https://ufsb.edu.br/remar/o-remar-cidadao \n OR \n http://crabdata.napier.ac.uk/public")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .accentColor(Color("REMAR_GREEN"))
+                    
+                    VStack(spacing: 20){
+                        Button(action: {QuestionManager.currentQuestion=0}, label: {standardButtonView_Text(text: "RETURN TO FIRST PAGE")})
+                    }
+                      .frame(width: geom.size.width*0.9, height: geom.size.height*0.07)
+                      .padding()
+                    
+
+                }.frame(width: geom.size.width*0.9)
+                  .ignoresSafeArea()
+                
             }
         }
         .navigationBarBackButtonHidden(true)
