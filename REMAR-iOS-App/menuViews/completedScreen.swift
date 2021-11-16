4b18313c204c15f98dbb78ee89a26e3ba4ee593f
lewiswatson55 <lewiswatson55@protonmail.com>
2021-11-16 22:49:21 +0000

    Added "Thank you for participating" splashscreen

--- REMAR-iOS-App/menuViews/completedScreen.swift
+++ REMAR-iOS-App/menuViews/completedScreen.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct completedScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geom in
+            ZStack{
+                //  Draw Background View
+                Rectangle().foregroundColor(.white)
+                uiBackgroundView()
+                
+                VStack{
+                    Spacer()
+                    Text("Done!")
+                        .font(.title)
+                        .padding(.bottom, 20)
+                    Text("Thank you for participating!")
+                    Text("You are now a citizen scientist!").bold()
+                    Image("crabs_both")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geom.size.width*0.8)
+                    Spacer()
+                }.padding(.top, 20)
+            }
+        }
     }
 }
 
--- REMAR-iOS-App/questionViews/finalSubmissionView.swift
+++ REMAR-iOS-App/questionViews/finalSubmissionView.swift
@@ -9,36 +9,49 @@ import SwiftUI
 
 struct finalSubmissionView: View {
     @EnvironmentObject var QuestionManager: questionManager
+    @State var showSplash = true
     
     var body: some View {
-        GeometryReader { geom in
-            ZStack {
-                uiBackgroundView()
-                VStack{
+        ZStack{
+            GeometryReader { geom in
+                ZStack {
+                    uiBackgroundView()
+                    VStack{
+                        
+                        
+                        
+                        Text("\(NSLocalizedString("FinalView", comment: ""))\n https://ufsb.edu.br/remar/o-remar-cidadao \n \(NSLocalizedString("Or", comment: "")) \n http://crabdata.napier.ac.uk/public")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                            .accentColor(Color("REMAR_GREEN"))
+                        
+                        VStack(spacing: 20){
+                            Button(action: {QuestionManager.currentQuestion=0}, label: {standardButtonView_Text(text: NSLocalizedString("ReturnRoot", comment: ""))})
+                        }
+                        .frame(width: geom.size.width*0.9, height: geom.size.height*0.07)
+                        .padding()
+                        
+                        
+                    }.frame(width: geom.size.width*0.9)
+                        .ignoresSafeArea()
                     
-                    
-                    
-                    Text("\(NSLocalizedString("FinalView", comment: ""))\n https://ufsb.edu.br/remar/o-remar-cidadao \n \(NSLocalizedString("Or", comment: "")) \n http://crabdata.napier.ac.uk/public")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                        .accentColor(Color("REMAR_GREEN"))
-                    
-                    VStack(spacing: 20){
-                        Button(action: {QuestionManager.currentQuestion=0}, label: {standardButtonView_Text(text: NSLocalizedString("ReturnRoot", comment: ""))})
-                    }
-                      .frame(width: geom.size.width*0.9, height: geom.size.height*0.07)
-                      .padding()
-                    
-
-                }.frame(width: geom.size.width*0.9)
-                  .ignoresSafeArea()
-                
+                }
             }
+            .navigationBarBackButtonHidden(true)
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
+            
+            completedScreen()
+                .opacity(showSplash ? 1 : 0)
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + splashscreen_time) {
+                        withAnimation(){
+                            self.showSplash = false
+                        }
+                    }
+                }
         }
-        .navigationBarBackButtonHidden(true)
-        .navigationBarTitle("")
-        .navigationBarHidden(true)
     }
 }
 
