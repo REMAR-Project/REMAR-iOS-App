0bee8bc665ce3516113db7ee26a8af780826fd14
lewiswatson55 <lewiswatson55@protonmail.com>
2021-12-17 00:18:17 +0000

    Added confirmation prompt on "testing app" button

--- REMAR-iOS-App/questionViews/SubmissionCheckView.swift
+++ REMAR-iOS-App/questionViews/SubmissionCheckView.swift
@@ -14,6 +14,8 @@ struct SubmissionCheckView: View {
     @Environment(\.managedObjectContext) var viewContext
     @FetchRequest(entity: UserData.entity(), sortDescriptors: []) var StoredData: FetchedResults<UserData>
     
+    @State private var showingAlert = false
+    
     var body: some View {
         GeometryReader { geom in
             ZStack {
@@ -24,7 +26,7 @@ struct SubmissionCheckView: View {
                         
                         Button(action: {QuestionManager.reverseAction()}, label: {standardButtonView_Text(text: NSLocalizedString("ChangeAnswers", comment: ""))})
                         
-                        Button(action: {QuestionManager.logAnswer()}, label: {standardButtonView_Text(text: NSLocalizedString("TestingAnswers", comment: ""))})
+                        Button(action: {showingAlert = true}, label: {standardButtonView_Text(text: NSLocalizedString("TestingAnswers", comment: ""))})
                         
                     }
                       .frame(width: geom.size.width*0.9, height: geom.size.height*0.3)
@@ -37,10 +39,19 @@ struct SubmissionCheckView: View {
                         .accentColor(Color("REMAR_GREEN"))
                     
                 }.frame(width: geom.size.width*0.9)
-                  .ignoresSafeArea()
+                    .ignoresSafeArea()
                 //.frame(width: geom.size.width, height: geom.size.height*0.98, alignment: .top)
                 uiBackgroundView()
                 
+            }.alert(isPresented:$showingAlert) {
+                Alert(
+                    title: Text(NSLocalizedString("alerttitle", comment: "")),
+                    message: Text(NSLocalizedString("alertmessage", comment: "")),
+                    primaryButton: .destructive(Text(NSLocalizedString("alertcontinue", comment: ""))) {
+                        QuestionManager.logAnswer()
+                    },
+                    secondaryButton: .cancel(Text(NSLocalizedString("alertback", comment: "")))
+                )
             }
         }
         .navigationBarBackButtonHidden(true)
@@ -71,6 +82,11 @@ struct standardButtonView_Text: View {
         }
         //.frame(width: geom.size.width*0.92, height: 45)
     }
+}
+
+func simulate_submission() {
+    @EnvironmentObject var QuestionManager: questionManager
+    QuestionManager.logAnswer()
 }
 
 struct SubmissionCheckView_Previews: PreviewProvider {
